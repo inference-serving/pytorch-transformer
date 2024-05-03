01a02b3e1a910225d4639ea34f2f98df3274152b
Saeid <s.ghafouri@qub.ac.uk>
2024-05-03 17:06:00 +0000

    midway implementing my version from the video

---
+++ saeid-code/train.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = Path(config["tokenizer_file"].format(lang))
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=[""])
